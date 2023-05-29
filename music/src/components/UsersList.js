3fa8aceb16c98e2b8c6755891b77c35684d7b005
aprotasa <aprotasa@ford.com>
2023-05-29 11:15:28 +0100

    Adding Thunks

--- music/src/components/UsersList.js
+++ music/src/components/UsersList.js
@@ -1,4 +1,13 @@
+import { fetchUsers } from "../store"; 
+import { useDispatch } from 'react-redux';
+import { useEffect } from "react";
+
 function UsersList() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, [dispatch]);
   return 'Users List';
 }
 
--- music/src/store/index.js
+++ music/src/store/index.js
@@ -6,3 +6,17 @@ export const store = configureStore({
     users: usersReducer
   }
 });
+
+export * from './thunks/fetchUsers';
+
+//Ways of storing data
+//Denormalized Form
+//Easier to use if the data is already structured as you need it.
+//Good if your project has rock solid requirements that won't change
+//This is where all information is stored under an id in an array
+
+
+//Normalized Form
+//More flexible. with a little code, we cna easily figure out any relationship
+//Downside: More code to write.
+//Where you have two arrays linked by an ID.
